b7eaaf2d60a9214f5b61be9e2e28069c49d3d74d
Szy György <szygyuri@gmail.com>
2022-12-18 16:22:59 +0100

    new canMatch routing, standalone components (strange, needed to switch TS checker)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
-import { NgModule } from '@angular/core';
+import { NgModule, inject } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HelloComponent } from './hello/hello.component';
+import { HiComponent } from './hi/hi.component';
+import { LoginServiceService } from './login-service.service';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'hello',
+    component: HelloComponent,
+    canActivate: [() => inject(LoginServiceService).isLoggedIn()]
+  },
+  {
+    path: 'false',
+    component: HelloComponent,
+    canMatch: [() => inject(LoginServiceService).doNot()]
+  },
+  {
+    path: 'false',
+    component: HiComponent,
+  },
+  {
+    path: 'hi',
+    component: HiComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/hello/hello.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  standalone: true,
+  selector: 'app-hello',
+  template: `<div>HELLO!</div>`,
+})
+export class HelloComponent {}
